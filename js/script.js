cd3ee7259bfe2ad54dc60cc849455b8be44c3de1
petargruden <83637462+petargruden@users.noreply.github.com>
2021-07-14 18:50:13 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+window.addEventListener("scroll",function(){
+  var nav = document.querySelector("header");
+  nav.classList.toggle("opacity", window.scrollY > 20);
+  
+  
+})
+
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
