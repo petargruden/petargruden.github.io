574a1002f33fb4602e6486f7c8675b6796a68721
petargruden <83637462+petargruden@users.noreply.github.com>
2021-07-15 13:02:59 +0200

    hamburger fix

--- js/script.js
+++ js/script.js
@@ -5,6 +5,22 @@ window.addEventListener("scroll",function(){
   
 })
 
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
 
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
 
 
